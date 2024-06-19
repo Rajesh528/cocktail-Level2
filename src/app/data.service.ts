2456af97e467d3ccb96324bb678acd1d8d45fc0e
Rajesh528 <rajeshkumar.mithinti@gmail.com>
2024-06-19 16:24:21 +0530

    Level 2 cocktail application

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,49 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Cocktail } from './cocktail';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   private dataSubject = new BehaviorSubject<Cocktail[]>([]);
+  private localStorageKey = 'myData'; // Key for local storage
   constructor(public http: HttpClient) {
     this.getCocktails();
   }
   private getCocktails(): void {
-    this.http.get<Cocktail[]>('/cockails').subscribe(
-      (data) => {
-        this.dataSubject.next(data);
-      },
-      (error) => {
-        console.error('Error fetching data:', error);
-      }
-    );
+    const storedData = localStorage.getItem(this.localStorageKey);
+    if (storedData) {
+      this.dataSubject.next(JSON.parse(storedData));
+    } else {
+      this.http.get<Cocktail[]>('/cockails').subscribe(
+        (data) => {
+          this.dataSubject.next(data);
+          localStorage.setItem(this.localStorageKey, JSON.stringify(data));
+        },
+        (error) => {
+          console.error('Error fetching data:', error);
+        }
+      );
+    }
   }
 
   getData(): Observable<Cocktail[]> {
     return this.dataSubject.asObservable();
   }
-  getCocktailById(id:string) {
+  getCocktailById(id: string) {
     const currentValue = this.dataSubject.getValue();
-    const updatedIndex = currentValue.findIndex(item => item.id === id);
-    console.log(currentValue[updatedIndex]);
-    return currentValue[updatedIndex]
+    const updatedIndex = currentValue.findIndex((item) => item.id === id);
+    return currentValue[updatedIndex];
   }
-  updateCocktail(updatedCocktail:Cocktail){
+  updateCocktail(updatedCocktail: Cocktail) {
     const currentValue = this.dataSubject.getValue();
-    const updatedIndex = currentValue.findIndex(item => item.id === updatedCocktail.id);
+    const updatedIndex = currentValue.findIndex(
+      (item) => item.id === updatedCocktail.id
+    );
     if (updatedIndex !== -1) {
       const updatedData = [...currentValue];
       updatedData[updatedIndex] = { ...updatedCocktail };
-      this.dataSubject.next(updatedData)
-      this.updateData(updatedData);
+      this.dataSubject.next(updatedData);
+      localStorage.setItem(this.localStorageKey, JSON.stringify(updatedData));
     }
-
- 
-  }
-  updateData(updatedData: any): Observable<any> {
-    return this.http.put<any>('/cockails', updatedData);
   }
 }
